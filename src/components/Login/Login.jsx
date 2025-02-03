e1dc78b5709ee839daa76d2632e2638b935c8587
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-03 16:36:27 +0600

    firebase add google login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../Firebase/firebase.init";
 
 
 const Login = () => {
+
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log('ERROR', error);
+            })
+    }
+
+
     return (
         <div>
             <div className="Container text-center mx-50% mt-20">
-                <button className="btn btn-accent">Login With Google</button>
+                <button onClick={handleGoogleSignIn} className="btn btn-accent">Login With Google</button>
             </div>
         </div>
     );
